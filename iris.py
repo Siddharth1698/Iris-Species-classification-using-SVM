0e06168467cd9cdc3dcacdcba46333b196cc4c4b
Siddharth M <siddharth.m98@gmail.com>
2019-08-11 22:10:11 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -13,7 +13,7 @@ X = dataset.iloc[:, [1,2,3,4]].values
 y = dataset.iloc[:, 5].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -23,7 +23,10 @@ X_test = sc_X.transform(X_test)
 # Fitting SVM to the Training set
 # Fitting SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+  decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+  max_iter=-1, probability=False, random_state=None, shrinking=True,
+  tol=0.001, verbose=False)
 classifier.fit(X_train, y_train)
 
 
